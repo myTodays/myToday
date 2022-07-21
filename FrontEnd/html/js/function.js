d8cb9e8bbb83d7a591d61a85a9c8a817b59df035
Melikui <melikui@qq.com>
2022-07-21 22:38:34 +0800

    js

--- FrontEnd/html/js/function.js
+++ FrontEnd/html/js/function.js
@@ -12,6 +12,23 @@ Function.prototype.myCall() = function (content) {
     let args = [...arguments].slice(1);
     content.fn = this;
     let result = content.fn(...args);
+    delete content.fn;
     return result;
+}
 
+// 手写apply
+Function.prototype.myApply() = function (content) {
+    if (typeof this != 'function') {
+        throw new Error('error');
+    }
+    content = content || window;
+    content.fn = this;
+    let result;
+    if(arguments[1]){
+        result = content.fn(...arguments[1]);
+    }else{
+        result = content.fn()
+    }
+    delete content.fn;
+    return result;
 }
